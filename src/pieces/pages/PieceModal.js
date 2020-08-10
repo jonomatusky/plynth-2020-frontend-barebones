78f7f3bd56f12811c05cdb007b420ddd8ae8a5cc
Jono Matusky <jono.matusky@gmail.com>
2020-08-10 06:52:18 -0400

    Add fullscreen button to piece page

--- src/pieces/pages/PieceModal.js
+++ src/pieces/pages/PieceModal.js
@@ -3,12 +3,29 @@ import React from 'react'
 
 import PieceCard from '../components/PieceCard'
 import PrimaryModal from '../../shared/layouts/PrimaryModal'
+import { Grid, Button } from '@material-ui/core'
+import { useHistory } from 'react-router-dom'
 
 // need to change loggedOut to auth instead of props
 const PieceModal = ({ open, onClose, piece }) => {
+  const history = useHistory()
+
   return (
     <PrimaryModal open={open} onClose={onClose}>
-      <PieceCard piece={piece} onClose={onClose} />
+      <Grid container direction="column" alignItems="center">
+        <Grid item>
+          <PieceCard piece={piece} onClose={onClose} />
+        </Grid>
+        <Grid item>
+          <Button
+            onClick={() => {
+              history.push(`/pieces/${piece.id}`)
+            }}
+          >
+            View Full Screen
+          </Button>
+        </Grid>
+      </Grid>
     </PrimaryModal>
   )
 }
