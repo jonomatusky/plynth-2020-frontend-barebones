4671262d7cd7203203996e9c857ba1b077441f3b
Jono Matusky <jono.matusky@gmail.com>
2020-09-15 07:14:30 -0400

    Add password change page

---
+++ src/users/components/SetPasswordForm.js
@@ -0,0 +1,108 @@
+import React, { useState } from 'react'
+import { Grid } from '@material-ui/core'
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+
+import { useApiClient } from '../../shared/hooks/api-hook'
+import ErrorBar from '../../shared/components/UIElements/ErrorBar'
+import MessageBar from '../../shared/components/UIElements/MessageBar'
+
+import { TextField } from '../../shared/components/FormElements/FormElements'
+
+import ActionButton from '../../shared/components/UIElements/ActionButton'
+
+const SetPasswordForm = props => {
+  const [success, setSuccess] = useState(false)
+  const { isLoading, error, sendRequest, clearError } = useApiClient()
+
+  const handleSubmit = async (values, { resetForm }) => {
+    delete values.passwordConfirmation
+
+    if (!isLoading) {
+      try {
+        const passwordData = { password: values }
+
+        await sendRequest(
+          `/auth/password`,
+          'PATCH',
+          JSON.stringify(passwordData)
+        )
+
+        resetForm()
+
+        if (!error) {
+          console.log('success!')
+          setSuccess(true)
+        }
+      } catch (err) {}
+    }
+  }
+
+  const initialValues = {
+    password: '',
+    newPassword: '',
+    passwordConfirmation: '',
+  }
+
+  const validationSchema = Yup.object({
+    password: Yup.string().min(
+      5,
+      'Password must be at least 5 characters long'
+    ),
+    newPassword: Yup.string().min(
+      5,
+      'Password must be at least 5 characters long'
+    ),
+    passwordConfirmation: Yup.string()
+      .min(5, 'Password must be at least 5 characters long')
+      .oneOf([Yup.ref('newPassword'), null], 'Passwords must match'),
+  })
+
+  return (
+    <React.Fragment>
+      <ErrorBar open={!!error} error={error} handleClose={clearError} />
+      <MessageBar
+        open={success}
+        message="Your password has been updated"
+        handleClose={() => setSuccess(false)}
+      />
+      <Formik
+        enableReinitialize="true"
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <Grid container direction="column" spacing={1}>
+            <Grid item>
+              <TextField
+                name="password"
+                label="Current Password"
+                type="password"
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                name="newPassword"
+                label="New Password"
+                type="password"
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                name="passwordConfirmation"
+                label="Confirm New Password"
+                type="password"
+              />
+            </Grid>
+            <Grid item>
+              <ActionButton type="submit" label="Submit" loading={isLoading} />
+            </Grid>
+          </Grid>
+        </Form>
+      </Formik>
+    </React.Fragment>
+  )
+}
+
+export default SetPasswordForm
---
+++ src/users/pages/ChangePassword.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { Container, Grid, Box, Typography } from '@material-ui/core'
+
+import PageTitle from '../../shared/components/UIElements/PageTitle'
+import Background from '../../shared/components/UIElements/Background'
+import SetPasswordForm from '../components/SetPasswordForm'
+
+import {
+  PieceBox,
+  BarRow,
+} from '../../shared/components/UIElements/CardSections'
+
+// need to change loggedOut to auth instead of props
+const ChangePassword = () => {
+  const history = useHistory()
+
+  return (
+    <React.Fragment>
+      <Background />
+      <Container maxWidth="xs">
+        <Grid container justify="flex-start" direction="column" spacing={2}>
+          <Box height="5vh"></Box>
+          <Grid item>
+            <PageTitle title="Set Password" />
+            <Typography>Change your password</Typography>
+          </Grid>
+          <Grid item>
+            <PieceBox container direction="column">
+              <BarRow
+                buttonLabel="Cancel X"
+                onClick={() => {
+                  history.push('admin/profile')
+                }}
+              />
+              <Grid container justify="center" alignItems="center">
+                <Grid item xs={11}>
+                  <Box height="0.75rem"></Box>
+                  <SetPasswordForm />
+                  <Box height="0.75rem"></Box>
+                </Grid>
+              </Grid>
+            </PieceBox>
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
+  )
+}
+
+export default ChangePassword
