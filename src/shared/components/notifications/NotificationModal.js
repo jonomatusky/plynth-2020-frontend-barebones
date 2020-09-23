907e95d6f0c760d396dd29cdab385a4842474873
Jono Matusky <jono.matusky@gmail.com>
2020-09-23 12:47:01 -0400

    Clean up

--- src/shared/components/notifications/NotificationModal.js
+++ src/shared/components/notifications/NotificationModal.js
@@ -1,16 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 import {
   Dialog,
   DialogTitle,
   DialogContent,
   DialogActions,
-  Grid,
-  Box,
-  Typography,
-  Paper,
 } from '@material-ui/core'
-import { PieceBox, BarRow } from '../ui/CardSections'
 import ActionButton from '../ui/ActionButton'
 
 const NotificationModal = ({
@@ -40,33 +35,33 @@ const NotificationModal = ({
           label={primaryActionLabel}
         />
       </DialogActions>
-      {/* <PieceBox container direction="column">
-        <BarRow buttonLabel="Cancel X" onClick={handleClose} />
-        <Paper>
-          <Grid container justify="center" alignItems="center">
-            <Box height="0.75rem"></Box>
-            <Grid item xs={11}>
-              <Typography variant="h5">{primaryMessage}</Typography>
-            </Grid>
-            <Grid item xs={11}>
-              <Typography>{secondaryMessage}</Typography>
-            </Grid>
-            <Grid item xs={11}>
-              <ActionButton
-                onClick={primaryAction}
-                label={primaryActionLabel}
-              />
-            </Grid>
-          </Grid>
-        </Paper> */}
-
-      {/* <ActionButton
-              variant="text"
-              onClick={secondaryAction}
-              label={secondaryActionLabel}
-            /> */}
-      {/* </PieceBox> */}
     </Dialog>
+    // {/* <PieceBox container direction="column">
+    //   <BarRow buttonLabel="Cancel X" onClick={handleClose} />
+    //   <Paper>
+    //     <Grid container justify="center" alignItems="center">
+    //       <Box height="0.75rem"></Box>
+    //       <Grid item xs={11}>
+    //         <Typography variant="h5">{primaryMessage}</Typography>
+    //       </Grid>
+    //       <Grid item xs={11}>
+    //         <Typography>{secondaryMessage}</Typography>
+    //       </Grid>
+    //       <Grid item xs={11}>
+    //         <ActionButton
+    //           onClick={primaryAction}
+    //           label={primaryActionLabel}
+    //         />
+    //       </Grid>
+    //     </Grid>
+    //   </Paper> */}
+
+    // {/* <ActionButton
+    //         variant="text"
+    //         onClick={secondaryAction}
+    //         label={secondaryActionLabel}
+    //       /> */}
+    // {/* </PieceBox> */}
   )
 }
 
